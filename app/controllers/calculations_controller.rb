1d14bc87f6a3d14910288057e2c3370a9e43f838
Greg Feldmann <gregory.j.feldmann@chicagobooth.edu>
2017-07-17 16:17:37 +0000

    Part II

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -9,4 +9,64 @@ class CalculationsController < ApplicationController
         render("calculations/flexible_square_template.html.erb")
     end
     
+    def flexible_square_root 
+    
+      @user_number = params["a_number"].to_i
+       
+       @square_root = @user_number**2
+         
+        
+        render("calculations/flexible_square_root_template.html.erb")
+    end    
+    
+    def flexible_payment
+    
+      @bps_input = params["basis_points"].to_i
+       
+      @bps = @bps_input/100
+    
+     @years_input = params["number_of_years"].to_i    
+        
+        @principal_input = params["present_value"].to_i
+        
+        @payment = (@bps*@principal_input)/(1-(1+@bps)**@years_input).to_i
+        
+        
+    # P = (r*PV)/(1-(1+r)^-n    
+        
+        render("calculations/flexible_payment_template.html.erb")
+    end    
+    
+    def min_max
+    
+      @min_input = params["min"].to_i
+      
+      @max_input = params["max"].to_i
+      
+      @random_number = rand(@min_input..@max_input)
+    
+     render("calculations/min_max_template.html.erb")
+     
+ end
+ 
+    def square_new
+        
+       @user_number = params["a_number"].to_i
+       
+       @squared_number = @user_number**2
+        
+        render("calculations/square_new_template.html.erb")
+    end    
+    
+    def process_square
+        
+         @the_number = params["the_user_number"].to_f
+       
+         @the_square = @the_number**2
+    
+        
+          render("calculations/process_square_template.html.erb")
+    end
+    
+ 
 end    
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,18 @@ Rails.application.routes.draw do
   
   get("/flexible/square/:a_number", { :controller => "calculations", :action => "flex_square"})
   
-  get("/square/new", { :controller => "calculations", :action => "square_form"})
+  # get("/square/new", { :controller => "calculations", :action => "square_form"})
   
+    get("/flexible/square/root/:a_number", { :controller => "calculations", :action => "flexible_square_root"})
+    
+   get("/flexible/payment/:basis_points/:number_of_years/:present_value", { :controller => "calculations", :action => "flexible_payment"})  
+   
+   get("/flexible/random/:min/:max", { :controller => "calculations", :action => "min_max"})
+   
+   get("/square/new", { :controller => "calculations", :action => "square_new"})
+   
+   get("/square/results", { :controller => "calculations", :action => "process_square"})
+   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount WebGit::Engine, at: "/rails/git"
 end
